607772f49919f938f7b5b83664e5e6a539c654bd
DanielBelenski <60483819+DanielBelenski@users.noreply.github.com>
2020-03-27 18:51:25 -0400

    Create Validator.java
    Created empty validator class to help with logic later on.

---
+++ app/src/main/java/com/example/dbelenskiregisterandlogin/Validator.java
@@ -0,0 +1,25 @@
+package com.example.dbelenskiregisterandlogin;
+
+public class Validator {
+    public Validator(){
+
+    }
+    public void isEmpty(){
+
+    }
+    public void uNameValidate(){
+
+    }
+    public void passValidate(){
+
+    }
+    public void nameValidate(){
+
+    }
+    public void emailValidate(){
+
+    }
+    public void dateValidate(){
+
+    }
+}
