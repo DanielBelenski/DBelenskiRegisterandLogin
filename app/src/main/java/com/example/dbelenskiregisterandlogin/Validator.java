431c98571e501e84eadd5ef80b20d94d63621ef9
DanielBelenski <60483819+DanielBelenski@users.noreply.github.com>
2020-03-29 20:16:52 -0400

    Plugin update and initial validation logic
    updated andoid studio and associated plugins.
    wrote initial validation logic, can check if fields have valid info but does not queary database yet.

--- app/src/main/java/com/example/dbelenskiregisterandlogin/Validator.java
+++ app/src/main/java/com/example/dbelenskiregisterandlogin/Validator.java
@@ -1,25 +1,80 @@
 package com.example.dbelenskiregisterandlogin;
 
+import java.util.Calendar;
+
 public class Validator {
     public Validator(){
 
     }
-    public void isEmpty(){
-
+    public Boolean isEmpty(String s){
+        if (s.length() == 0)
+            return false;
+        else{
+            return true;
+        }
     }
-    public void uNameValidate(){
 
+    public Boolean uNameValidate(String s){
+        //TODO: check database for username
+        if (s.length() < 5){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
-    public void passValidate(){
-
+    public Boolean registerPassValidate(String s){
+        if (s.length() < 5){
+            return false;
+        }
+        Boolean num = false;
+        for(int i=0; i < s.length(); i++){
+            if (Character.isDigit(s.charAt(i))){
+                num = true;
+            }
+        }
+        if (num){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    public void nameValidate(){
-
+    public Boolean loginPassValidate(String s){
+        //TODO: need to query database for password
+        return true;
     }
-    public void emailValidate(){
-
+    public Boolean nameValidate(String s){
+        if(s.length() < 3){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
-    public void dateValidate(){
-
+    public Boolean emailValidate(String s){
+        Boolean atChar = false;
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '@' && i > 3 && i <= s.length()-9){
+                atChar = true;
+            }
+        }
+        return atChar;
+    }
+    public Boolean dateValidate(String s){
+        int curYear = Calendar.getInstance().get(Calendar.YEAR);
+        int curMonth = Calendar.getInstance().get(Calendar.MONTH);
+        String[] date = s.split("/", 3);
+        int givYear = Integer.parseInt(date[2]);
+        int givMonth = Integer.parseInt(date[0]);
+        if (curYear - givYear >= 18){
+            return true;
+        }
+        else if (curYear - givYear == 17 && curMonth - givMonth >= 0){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
